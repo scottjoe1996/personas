70589e5b043c715f72989022d9cd97d4be3875ca
Joseph Scott <j.scott@resilientplc.com>
2022-07-04 08:34:25 +0100

    created config factory that returns personaService

---
+++ app/resolvers/config-factory.ts
@@ -0,0 +1,15 @@
+import { PersonaService } from './services/persona-service/persona-service';
+import { DynamoPersonaDataInterface } from './data-interfaces/persona-data-interface/dynamo-persona-data-interface';
+import { DynamoDB } from 'aws-sdk';
+
+interface Config {
+  personaService: PersonaService;
+}
+
+export class ConfigFactory {
+  public createConfig(): Config {
+    const personaInterface = new DynamoPersonaDataInterface(new DynamoDB({ apiVersion: '2012-08-10' }), process.env.DYNAMO_DB_TABLE_NAME as string);
+
+    return { personaService: new PersonaService(personaInterface) };
+  }
+}
